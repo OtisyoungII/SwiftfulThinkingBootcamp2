f329971651f41ae0ecff7981061e02b2a5bd649b
Otis YII <oyoung21@gmail.com>
2024-11-15 09:15:34 -0500

    Initial Commit

---
+++ SwiftfulThinkingBootcamp2/SwiftfulThinkingBootcamp2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcamp2App.swift
+//  SwiftfulThinkingBootcamp2
+//
+//  Created by Otis Young on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcamp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
